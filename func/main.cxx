fc49f8510e4d96a074d7916583a6712493098fba
Matthew Strait <strait@physics.umn.edu>
2017-09-27 09:16:36 -0500

    Trivial: spelling

--- func/main.cxx
+++ func/main.cxx
@@ -34,7 +34,7 @@ Code style:
 #include "event.h"
 
 struct DRAWPARS{
-  // The ticks to draw right now, typically just the minumum needed
+  // The ticks to draw right now, typically just the minimum needed
   // and not the whole range that is visible.
   int32_t firsttick, lasttick;
   bool clear;
