99934e810a88816fa8c82ae5b80d818954e0a0a9
Matthew Strait <strait@physics.umn.edu>
2017-10-20 14:19:58 -0500

    Find screen track points once.
    Instead of finding the screen positions of track points
    twice for every point except the first and last by finding
    them within draw_trackseg, find them all first and then
    pass the results in.
    
    This is not done so much for efficiency as so that the
    positions can later be cached and used for mouseover features.

--- func/tracks.cxx
+++ func/tracks.cxx
@@ -9,29 +9,23 @@ extern std::vector<noeevent> theevents;
 extern int gevi;
 extern int pixx, pixy;
 
-static void draw_trackseg(cairo_t * cr, const trackpoint & point1,
-                                        const trackpoint & point2)
+static std::pair<int, int> trackpoint_to_screen(const trackpoint & tp)
 {
-  if(point1.plane%2 ^ point2.plane%2) return; // shouldn't happen
+  return std::pair<int, int>(
+    det_to_screen_x(tp.plane)          + (0.5 + tp.fplane)*pixx/2,
+    det_to_screen_y(tp.plane, tp.cell) + (0.5 - tp.fcell )*pixy
+  );
+}
 
-  const int screenx1 = det_to_screen_x(point1.plane)
-                     + (0.5 + point1.fplane)*pixx/2;
-  const int screenx2 = det_to_screen_x(point2.plane)
-                     + (0.5 + point2.fplane)*pixx/2;
-
-  const int screeny1 = det_to_screen_y(point1.plane, point1.cell)
-                     + (0.5 - point1.fcell)*pixy;
-  const int screeny2 = det_to_screen_y(point2.plane, point2.cell)
-                     + (0.5 - point2.fcell)*pixy;
-
+static void draw_trackseg(cairo_t * cr, const std::pair<int, int> & point1,
+                                        const std::pair<int, int> & point2)
+{
   /* Do not try to optimize by not drawing track segments that are entirely out
      of the view, because I don't want to do the work, and I suspect the
      performance advantage is small in most cases (but haven't checked). */
-
   cairo_set_source_rgb(cr, 0, 1, 1);
-
-  cairo_move_to(cr, screenx1, screeny1);
-  cairo_line_to(cr, screenx2, screeny2);
+  cairo_move_to(cr, point1.first, point1.second);
+  cairo_line_to(cr, point2.first, point2.second);
   cairo_stroke(cr);
 }
 
@@ -39,8 +33,12 @@ static void draw_tracks_in_one_view(cairo_t * cr,
                                    const std::vector<trackpoint> & traj)
 {
   if(traj.size() < 2) return;
+  std::vector< std::pair<int, int> > screenpoints;
+  for(unsigned int h = 0; h < traj.size(); h++)
+    screenpoints.push_back(trackpoint_to_screen(traj[h]));
+
   for(unsigned int h = 0; h < traj.size()-1; h++)
-    draw_trackseg(cr, traj[h], traj[h+1]);
+    draw_trackseg(cr, screenpoints[h], screenpoints[h+1]);
 }
 
 void draw_tracks(cairo_t ** cr,
